2aebda7e3a4109f68e8df6d20f8cd8f0a07e4904
Andersmb <anders.brakestad@gmail.com>
2018-11-09 13:18:09 +0100

    first commit

---
+++ observations.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+observations = {
+    'forest': ['cow', 'cow', 'sheep', 'sheep', 'pig', 'sheep'],
+    'plains': ['horse', 'horse', 'sheep', 'horse'],
+    'jungle': ['ocelot', 'parrot', 'sheep', 'parrot'],
+}
+
+print(observations)
