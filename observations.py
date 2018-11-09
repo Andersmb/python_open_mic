62b17b9b5f1269c0988c273c19a37e81670c0885
Andersmb <anders.brakestad@gmail.com>
2018-11-09 15:33:30 +0100

    first commit

--- observations.py
+++ observations.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
-observations = {
+obs = {
     'forest': ['cow', 'cow', 'sheep', 'sheep', 'pig', 'sheep'],
     'plains': ['horse', 'horse', 'sheep', 'horse'],
     'jungle': ['ocelot', 'parrot', 'sheep', 'parrot'],
 }
 
-print(observations)
+# make new dict for number of unique observations in each biome
+unique_obs = {biome:len(set(animal)) for (biome,animal) in obs.items()}
+
+total_sheep_obs = 0
+for biome,animal in obs.items():
+	total_sheep_obs += animal.count("sheep")
+
+all_unique_obs = []
+for el in obs.values():
+	for animal in el:
+		if animal not in all_unique_obs:
+			all_obs.append(animal)
+
+
