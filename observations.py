922d6c77c5b52a24434a3bda6dc1c268178af53e
Andersmb <anders.brakestad@gmail.com>
2018-11-09 16:22:01 +0100

    two questions remaining to answer

--- observations.py
+++ observations.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 obs = {
-    'forest': ['cow', 'cow', 'sheep', 'sheep', 'pig', 'sheep'],
-    'plains': ['horse', 'horse', 'sheep', 'horse'],
-    'jungle': ['ocelot', 'parrot', 'sheep', 'parrot'],
+    'forest': ['cow', 'sheep', 'sheep', 'pig', 'sheep'],
+    'plains': ['cow', 'horse', 'horse', 'sheep', 'horse'],
+    'jungle': ['cow', 'ocelot', 'parrot', 'sheep', 'parrot'],
 }
 
 # make new dict for number of unique observations in each biome
@@ -16,25 +16,36 @@ for biome,animal in obs.items():
 
 # make a list that contains all unique animal observations across biomes
 all_unique_obs = []
+all_obs = []
 for el in obs.values():
 	for animal in el:
+		all_obs.append(animal)
 		if animal not in all_unique_obs:
 			all_unique_obs.append(animal)
 
-# check if some animal was observed in all biomes
-in_all = False
+# check if some animal was observed in all biomes and count observations of animals
+in_all = []
 t = []
+counts = []
 for key in obs.keys():
 	for animal in all_unique_obs:
+
+		# count all and unique observations
+		counts.append((all_obs.count(animal), animal))
+		
 		if animal not in obs[key]:
 			continue
 		else:
 			t.append((animal))
 for animal in all_unique_obs:
 	if t.count(animal) == len(obs):
-		in_all = (animal, True)
+		in_all.append((animal, True))
 
+# get rid of duplicates
+counts = sorted(set(counts))
 
+# determine the most frequently observed animal
+most_freq_animal = max(counts)[1]
 
 
 
