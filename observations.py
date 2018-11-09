06ea65c4f7c2a70633b50779c89e1de53fb0dc35
Andersmb <anders.brakestad@gmail.com>
2018-11-09 15:59:36 +0100

    3 questions remaining to answer

--- observations.py
+++ observations.py
@@ -9,14 +9,33 @@ obs = {
 # make new dict for number of unique observations in each biome
 unique_obs = {biome:len(set(animal)) for (biome,animal) in obs.items()}
 
+# Count total number of sheep observations
 total_sheep_obs = 0
 for biome,animal in obs.items():
 	total_sheep_obs += animal.count("sheep")
 
+# make a list that contains all unique animal observations across biomes
 all_unique_obs = []
 for el in obs.values():
 	for animal in el:
 		if animal not in all_unique_obs:
-			all_obs.append(animal)
+			all_unique_obs.append(animal)
+
+# check if some animal was observed in all biomes
+in_all = False
+t = []
+for key in obs.keys():
+	for animal in all_unique_obs:
+		if animal not in obs[key]:
+			continue
+		else:
+			t.append((animal))
+for animal in all_unique_obs:
+	if t.count(animal) == len(obs):
+		in_all = (animal, True)
+
+
+
+
 
 
