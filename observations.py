5c1c4e04e637a2ccd211a38a5a93af607339c1e4
Andersmb <anders.brakestad@gmail.com>
2018-11-09 21:40:54 +0100

    one question remaining to answer

--- observations.py
+++ observations.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import pandas
 
 obs = {
     'forest': ['cow', 'sheep', 'sheep', 'pig', 'sheep'],
@@ -8,6 +9,7 @@ obs = {
 
 # make new dict for number of unique observations in each biome
 unique_obs = {biome:len(set(animal)) for (biome,animal) in obs.items()}
+print(f"Number of unique species in biomes: {unique_obs}")
 
 # Count total number of sheep observations
 total_sheep_obs = 0
@@ -39,14 +41,31 @@ for key in obs.keys():
 			t.append((animal))
 for animal in all_unique_obs:
 	if t.count(animal) == len(obs):
-		in_all.append((animal, True))
+		in_all.append(animal)
+
+print(f"Animal(s) observed in all biomes: {', '.join(in_all)}")
 
 # get rid of duplicates
 counts = sorted(set(counts))
 
 # determine the most frequently observed animal
 most_freq_animal = max(counts)[1]
+print(f"The most frequently observed animal was {most_freq_animal} ({max(counts)[0]} observations)")
+sheep_count = [animal[0] for animal in counts if "sheep" in animal][0]
 
+# count sheep specifically
+print(f"Sheep were observed in total {sheep_count} times.")
 
+# evaluate whether parrot only present in jungle
+parrot_test = []
+for biome in obs.keys():
+	if "parrot" in obs[biome]:
+		parrot_test.append(True)
+	else:
+		parrot_test.append(False)
 
+if parrot_test == [False, False, True]:
+	print("Parrot(s) were only observed in the jungle.")
+else:
+	print("Parrot(s) were not only observed in the jungle.")
 
